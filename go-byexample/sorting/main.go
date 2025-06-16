520228ab6ffb988b38cae63fc119c9dc263ee418
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2025-06-16 11:56:31 +0530

    Done with sorting

--- go-byexample/sorting/main.go
+++ go-byexample/sorting/main.go
@@ -1,1 +1,20 @@
-package sorting
+package main
+
+import (
+	"fmt"
+	"slices"
+	"sort"
+)
+
+func main() {
+	strs := []string{"a", "d", "c"}
+	sort.Strings(strs)
+	//slices.Sort(strs)
+	fmt.Println(strs)
+
+	arrayOfint := []int{2, 4, 5, -1, 32, 32, 246, -19}
+	slices.Sort(arrayOfint)
+	fmt.Println(arrayOfint)
+
+	fmt.Println(slices.IsSorted(arrayOfint))
+}
